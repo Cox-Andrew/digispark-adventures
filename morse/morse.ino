38aaf934c6f28ec7060ffd20a32925e53b943b1a
Andrew Cox <18695702+Cox-Andrew@users.noreply.github.com>
2023-08-15 15:42:17 +0100

    not quite working

--- morse/morse.ino
+++ morse/morse.ino
@@ -1,6 +1,4 @@
-#include <string>
-
-const int map[26] = {
+const int morse_map[26] = {
   0b10,
   0b0001,
   0b0101,
@@ -29,20 +27,64 @@ const int map[26] = {
   0b0011,
 };
 
-const string input = "HELLO";
+const int len = 16;
+const char words[len] = "SOS";
 
 // the setup routine runs once when you press reset:
-void setup() {                
+void setup() {
   // initialize the digital pin as an output.
   pinMode(1, OUTPUT); //LED on Model A
 }
 
+// single unit of time delay for morse
+#define UNIT 500
+
 // the loop routine runs over and over again forever:
 void loop() {
-  for (int i = 0; i < input.length)
-  digitalWrite(1, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(1, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+  // dot 1
+  // dash 3
+  // symbols 1
+  // letters 3
+  // words 7
+  
+  for (int i = 0; i < len; i++) {
+    // check that we haven't reached the end of the string
+    if (words[i] == '\0') break;
+    // check if we have space character
+    if (words[i] == ' ') {
+      delay(7 * UNIT);
+      continue;
+    }
+
+    int sequence = morse_map[words[i] - 'A'];
+
+    while (sequence) {
+      int is_dash = sequence & 1;
+      sequence >>= 1;
+
+      digitalWrite(1, HIGH);
+      if (is_dash) {
+        delay(3 * UNIT);
+      } else {
+        delay(UNIT);
+      }
+      digitalWrite(1, LOW);
+
+      // delay between symbols (only if still same sequence)
+      if (sequence) delay(UNIT);
+    }
+
+    // delay between letters (only if still same word)
+    if (words[i+1] != ' ') delay(3 * UNIT);
+  }
+
+  // we did the message WOOO, flash, wait a bit, repeat
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(1, HIGH);
+    delay(50);
+    digitalWrite(1, LOW);
+    delay(50);
+  }
+  delay(14 * UNIT);
 }
 
